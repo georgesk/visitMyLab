3dac4f174dfe3329f14662f4f11a4d616f67ec35
georgesk <georgesk@debian.org>
2015-03-03 10:51:15 +0100

    polished the tab UI

--- js/vml-main.js
+++ js/vml-main.js
@@ -1,11 +1,17 @@
+// The main style, tochoose a skin for the application
+// Following values are implemented: "tabs" (default style), ...
+var mainStyle="";
+
 // The .ready callback, which is the main javascript program
 // launched when the DOM is ready.
 
 $(
     function(){
-	$( "#tabs" ).tabs({activate: onTabActivate,});
-
-	createScopeWidget($( "#tabs" ), $('#experiments'));
+	if (! mainStyle) mainStyle="tabs";
+	if (mainStyle == "tabs") {
+	    $( "#tabs" ).tabs({activate: onTabActivate,});
+	    createScopeWidget($('#experiments'), $("#tabs"));
+	}
     }
 );
 
@@ -25,17 +31,30 @@ function onTabActivate(event, ui){
 
 /**
  * Creates the scope widget and tailors it shap to fit within a tab
- * @parameter tabs a jqueryfied element which implements tabs
  * @parameter container a jqueryfied element to contain the scope
+ * @parameter controlWidget an optional jqueryfied element which implements
+ *  a control widget like jQuery tabs
  **/
-function createScopeWidget(tabs, container){
+function createScopeWidget(container, controlWidget){
     /////////// creates the scope widget //////////////
     scope_widget(container);
     // scope's top must be moved down since its parent panel is not
     // positioned in absolute style.
-    container.css('top', parseInt(container.css('top'))+tabs.height());
-    // make the scope resizeable as it is initially sized from
-    // the tabs defaults.
-    container.resizable({handles:"s"});
+
+    if (controlWidget.attr("class").contains("ui-tabs")){
+	// then the controlWidget is created by "tabs", let us tailor
+	// the container's shape
+	var h=parseInt(controlWidget.height());
+	container.css({
+	    top: parseInt(container.css('top')) + h,
+	    bottom: "",
+	    height: parseInt($(window).height()) - h - 120,
+	});
+	// make the scope resizeable as it is initially sized from
+	// the tabs defaults.
+	container.resizable({handles:"s"});
+	// adds a title to resize handles
+	$(".ui-resizable-handle").attr("title","Drag here to resize the widget")
+    }
 }
 
